1ac42337159ac43eb97e033af33c93faab3ba2c0
Darkmanltu <154636891+Darkmanltu@users.noreply.github.com>
2024-11-30 22:11:53 +0000

    intital commit for lab3 proggress

--- src/Lib3.hs
+++ src/Lib3.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE InstanceSigs #-}
+{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
 module Lib3
     ( stateTransition,
     StorageOp (..),
@@ -9,8 +9,11 @@ module Lib3
     renderStatements
     ) where
 
-import Control.Concurrent ( Chan )
-import Control.Concurrent.STM(STM, TVar)
+import Control.Concurrent (  Chan, readChan, writeChan, newChan)
+import Control.Concurrent.STM(STM, TVar, stateTVar)
+import Control.Concurrent.STM (TVar, readTVar, atomically, writeTVar, readTVarIO)
+import Control.Exception (IOException, try)
+import Data.Either(isLeft)
 import qualified Lib2
 
 data StorageOp = Save String (Chan ()) | Load (Chan String)
@@ -21,8 +24,27 @@ data StorageOp = Save String (Chan ()) | Load (Chan String)
 -- to a channel provided in a request.
 -- Modify as needed.
 storageOpLoop :: Chan StorageOp -> IO ()
-storageOpLoop _ = do
-  return $ error "Not implemented 1"
+storageOpLoop chan = do
+    op <- readChan chan
+    case op of
+        Save content responseChan -> do
+            attempt <- try (writeFile "state.txt" content) ::  IO (Either IOException ())
+            case attempt of
+              Left er -> do
+                putStrLn $ "Error: " ++ show er
+              Right _ -> do
+                putStrLn "Saved successfully"
+            writeChan responseChan ()
+        Load responseChan -> do
+            attempt2 <- try (readFile "state.txt") :: IO (Either IOException String)
+            case attempt2 of
+              Left er -> do
+                putStrLn $ "Error: " ++ show er
+                writeChan responseChan ""
+              Right content -> do
+                putStrLn "Loaded successfully"
+                writeChan responseChan content
+    storageOpLoop chan -- Continue loop
 
 data Statements = Batch [Lib2.Query] |
                Single Lib2.Query
@@ -35,19 +57,52 @@ data Command = StatementCommand Statements |
 
 -- | Parses user's input.
 parseCommand :: String -> Either String (Command, String)
-parseCommand _ = Left "Not implemented 2"
-
+parseCommand input = 
+    case Lib2.parseWhitespaces input of
+        Right (cmd, rest) -> 
+            if cmd == "load" then
+                Right (LoadCommand, rest)
+            else if cmd == "save" then
+                Right (SaveCommand, rest)
+            else
+                case parseStatements input of
+                    Right (stmts, rest) -> Right (StatementCommand stmts, rest)
+                    Right (stmts, rest') -> Right (StatementCommand stmts, rest')
+        Left e1 -> Left e1
 -- | Parses Statement.
 -- Must be used in parseCommand.
 -- Reuse Lib2 as much as you can.
 -- You can change Lib2.parseQuery signature if needed.
 parseStatements :: String -> Either String (Statements, String)
-parseStatements _ = Left "Not implemented 3"
+parseStatements input = do
+  case Lib2.parseWhitespaces input of
+    Left err -> Left err
+    Right (_, rest) -> do
+      case Lib2.parseWord rest of
+        Left err -> Left err
+        Right (word, rest') -> do
+          if word == "BEGIN" then
+            case Lib2.parseQuery rest' of
+              Left err -> Left err
+              Right (rest'', query) -> do
+                if rest'' == "" then
+                  return (Single query, rest'')
+                else do
+                  case parseStatements rest'' of
+                    Left err -> Left err
+                    Right (Batch queries, remaining) -> return (Batch (query : queries), remaining)
+          else
+            case Lib2.parseQuery rest' of
+              Left err -> Left err
+              Right (rest'', query) -> return (Single query, rest'')
 
 -- | Converts program's state into Statements
 -- (probably a batch, but might be a single query)
 marshallState :: Lib2.State -> Statements
-marshallState _ = error "Not implemented 4"
+marshallState state =
+  let queries = [Lib2.ViewInventory]
+  in
+    Single (head queries)
 
 -- | Renders Statements into a String which
 -- can be parsed back into Statements by parseStatements
@@ -56,7 +111,50 @@ marshallState _ = error "Not implemented 4"
 -- Must have a property test
 -- for all s: parseStatements (renderStatements s) == Right(s, "")
 renderStatements :: Statements -> String
-renderStatements _ = error "Not implemented 5"
+renderStatements stmts =
+  case stmts of
+    Single query -> renderQuery query
+    Batch queries ->
+      "BEGIN " ++ concatMap renderQuery queries ++ "END " 
+
+
+
+renderQuery :: Lib2.Query -> String
+renderQuery query =
+  case query of
+    (Lib2.Buy item) -> "Buy " ++ showItemAsQuery item
+    (Lib2.Sell item) -> "Sell " ++ showItemAsQuery item
+    (Lib2.BuyBundle bundle) -> "AddBundle " ++ showBundleAsQuery bundle
+    (Lib2.ViewInventory) -> "ViewInventory"
+    Lib2.ViewInventory -> "ViewInventory"
+
+showItemAsQuery :: Lib2.Item -> String
+showItemAsQuery (Lib2.Item name price) = name ++ " " ++ showPriceAsQuery price
+
+
+showPriceAsQuery :: Lib2.ItemPrice -> String
+showPriceAsQuery (Lib2.SinglePrice amount currency) = show amount ++ " " ++ show currency
+
+showBundleAsQuery :: [Lib2.Item] -> String
+showBundleAsQuery items = "(" ++ concatMap showItemAsQuery items ++ ")"
+
+
+processSingleQuery :: TVar Lib2.State -> Lib2.Query -> STM (Either String (Maybe String))
+processSingleQuery givenState query = do
+      currentState <- readTVar givenState
+      case Lib2.stateTransition currentState query of
+        Right (msg, updatedState) -> do
+          writeTVar givenState updatedState
+          case msg of
+            Just str -> return $ Right $ Just str
+            Nothing -> return $ Right Nothing
+        Left err -> return $ Left err
+
+
+printQueryResponse :: Either String (Maybe String) -> IO ()
+printQueryResponse (Left err) = putStrLn $ "Failed: " ++ err
+printQueryResponse (Right Nothing) = putStrLn "Success: No message returned"
+printQueryResponse (Right (Just msg)) = putStrLn $ "Success: " ++ msg
 
 -- | Updates a state according to a command.
 -- Performs file IO via ioChan if needed.
@@ -70,4 +168,50 @@ renderStatements _ = error "Not implemented 5"
 -- is stored in transactinal variable
 stateTransition :: TVar Lib2.State -> Command -> Chan StorageOp ->
                    IO (Either String (Maybe String))
-stateTransition _ _ ioChan = return $ Left "Not implemented 6"
+stateTransition stateVar command ioChan = do
+  case command of
+    StatementCommand (Batch queries) -> do
+      results <- atomically $ mapM (processSingleQuery stateVar) queries
+      mapM_ printQueryResponse results
+      if any isLeft results
+        then return $ Left "Error processing batch of queries"
+        else return $ Right $ Just "Batch of queries processed successfully"
+
+
+    StatementCommand (Single query) -> do
+      result <- atomically $ processSingleQuery stateVar query
+      result <- atomically (processSingleQuery stateVar query)
+      _ <- printQueryResponse result
+      if isLeft result
+        then return $ Left "Error processing single query"
+        else return $ Right Nothing
+
+
+    LoadCommand -> do
+      responceChan <- newChan
+      writeChan ioChan (Load responceChan)
+      loadedStateAsString <- readChan responceChan
+      case parseStatements loadedStateAsString of
+        Right (parsedState, _) ->
+          case parsedState of
+            Single Lib2.ViewInventory -> do
+              atomically $ writeTVar stateVar Lib2.emptyState
+              return $ Right Nothing
+            _ -> return $ Left "Failed to load state from the file"
+        Left _ ->
+          return $ Left "Failed to parse the loaded state"
+
+
+    SaveCommand -> do
+      -- Getting state
+      currentState <- readTVarIO stateVar
+      let statementsAsString = renderStatements $ marshallState currentState
+      -- Creating Chan () for response
+      responceChan <- newChan
+      -- Sending save operation to channel
+      writeChan ioChan (Save statementsAsString responceChan)
+      -- Waiting for response
+      _ <- readChan responceChan
+      return $ Right $ Just "State saved"
+
+
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,33 +1,50 @@
 {-# LANGUAGE ImportQualifiedPost #-}
+{-# OPTIONS_GHC -Wno-deferred-out-of-scope-variables #-}
 import Test.Tasty ( TestTree, defaultMain, testGroup )
 import Test.Tasty.HUnit ( testCase, (@?=) )
-import Test.Tasty.QuickCheck as QC
 
-import Data.List
-import Data.Ord
 
-import Lib1 qualified
 import Lib2 qualified
+
+
 
 main :: IO ()
 main = defaultMain tests
 
 tests :: TestTree
-tests = testGroup "Tests" [unitTests, propertyTests]
+tests = testGroup "Tests" [unitTests]
 
 unitTests :: TestTree
-unitTests = testGroup "Lib1 tests"
-  [ testCase "List of completions is not empty" $
-      null Lib1.completions @?= False,
-    testCase "Parsing case 1 - give a better name" $
-      Lib2.parseQuery "" @?= (Left "Some error message"),
-    testCase "Parsing case 2 - give a better name" $
-      Lib2.parseQuery "o" @?= (Left "Some error message")
-  ]
-
-propertyTests :: TestTree
-propertyTests = testGroup "some meaningful name"
+unitTests = testGroup "Lib2 tests"
   [
-    QC.testProperty "sort == sort . reverse" $
-      \list -> sort (list :: [Int]) == sort (reverse list)
+    testCase "Valid buy command parsing" $
+      Lib2.parseQuery "Buy Sword 10 gold"
+        @?= Right (Lib2.Buy (Lib2.Item "Sword" (Lib2.SinglePrice 10 Lib2.Gold))),
+    testCase "Invalid Buy command with empty input" $
+       Lib2.parseQuery "" @?= Left "Failed to parse command: Invalid character: expected a word",
+    testCase "Unknown command" $
+      Lib2.parseQuery "Fly Sword 10 gold"
+        @?= Left "Unknown command: Fly",
+        
+    
+    testCase "ViewInventory command parsing" $
+      Lib2.parseQuery "ViewInventory"
+        @?= Right Lib2.ViewInventory,
+    
+    testCase "Valid sell command parsing" $
+      Lib2.parseQuery "Sell Potion 5 silver"
+        @?= Right (Lib2.Sell (Lib2.Item "Potion" (Lib2.SinglePrice 5 Lib2.Silver))),
+
+    testCase "Invalid sell command with unknown item" $
+      Lib2.parseQuery "Sell UnknownItem 5 gold"
+        @?= Left "Failed to parse Sell command: Failed to parse item: Invalid item name",
+
+    testCase "Simple bundle parsing with two items" $
+      Lib2.parseQuery "BuyBundle (Sword 5 gold and Shield 3 silver)"
+        @?= Right (Lib2.BuyBundle [Lib2.Item "Sword" (Lib2.SinglePrice 5 Lib2.Gold), Lib2.Item "Shield" (Lib2.SinglePrice 3 Lib2.Silver)]),
+
+    testCase "Nested bundle parsing with complex structure" $
+      Lib2.parseQuery "BuyBundle ((Sword 5 gold and Shield 3 silver) and (Potion 2 copper and Armor 4 gold))"
+        @?= Right (Lib2.BuyBundle [Lib2.Item "Sword" (Lib2.SinglePrice 5 Lib2.Gold), Lib2.Item "Shield" (Lib2.SinglePrice 3 Lib2.Silver), Lib2.Item "Potion" (Lib2.SinglePrice 2 Lib2.Copper), Lib2.Item "Armor" (Lib2.SinglePrice 4 Lib2.Gold)])
+
   ]
